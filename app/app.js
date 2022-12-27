b44c52d8cf5482b65a12442a676cb10b5d09a68a
Ruben S. Garcia <rssanjuan2704@gmail.com>
2022-12-27 13:59:17 +0300

    refactor: Change initialization place of routes
    This needs to be done after instructing Express to parse form requests
    in a JSON format.

--- app/app.js
+++ app/app.js
@@ -6,33 +6,40 @@ const cookieParser = require("cookie-parser");
 const logger = require("./logger");
 const routes = require("./routes");
 
+require("./database");
+require("./models");
+
 const app = express();
+
+app.use(logger);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use(logger);
-app.use(routes);
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(routes);
+
 // catch 404 and forward to error handler
 app.use(function (_, __, next) {
-  next(createError(404));
+  const err = createError(404);
+  console.log(`Status is: ${err.status}`);
+  next(err);
 });
 
 // error handler
-app.use(function (err, req, res) {
+// eslint-disable-next-line
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("error", { message: err.message, error: err });
 });
 
 module.exports = app;
