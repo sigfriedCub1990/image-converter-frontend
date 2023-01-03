92826e4dd325c1adc4f2c49704522472905abdb0
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-01-03 14:36:14 +0300

    refactor: Print possible mongoose errors

--- app/database.js
+++ app/database.js
@@ -4,7 +4,21 @@ const dbHost = process.env.DB_HOST;
 const dbPort = process.env.DB_PORT;
 const dbName = process.env.DB_NAME;
 
+const handleMongooseError = (message) => (error) => {
+  console.log(message);
+  console.error(error.message);
+};
+
+const handleError = handleMongooseError("Mongoose Error");
+const disconnectError = handleMongooseError("MongoDB disconnected error");
+
 mongoose.set("strictQuery", false);
-mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`);
+mongoose
+  .connect(`mongodb://${dbHost}:${dbPort}/${dbName}`)
+  .then(() => console.log(`Connected to: ${dbHost}:${dbPort}/${dbName}`))
+  .catch(handleError);
+
+mongoose.connection.on("error", handleError);
+mongoose.connection.on("disconnect", disconnectError);
 
 module.exports = mongoose;
