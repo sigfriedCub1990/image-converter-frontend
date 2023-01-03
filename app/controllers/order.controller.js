2433484160936679163bb109342768f7c9da07f1
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-01-03 14:26:00 +0300

    refactor: Remove obsolete files

--- app/controllers/order.controller.js
+++
@@ -1,5 +0,0 @@
-import { postOrder } from "../use-cases/orders";
-
-export default Object.freeze({
-  postOrder: (httpRequest) => postOrder(httpRequest, null), // null = postPaymentLink in original article
-});
