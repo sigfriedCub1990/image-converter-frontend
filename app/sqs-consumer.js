70f4b783df46808d8b15da714ad03e08f1b1b186
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-01-11 16:11:56 +0300

    feat: Consume message from SQS

---
+++ app/sqs-consumer.js
@@ -0,0 +1,33 @@
+const { Consumer } = require("sqs-consumer");
+const { imageDb } = require("./data-access");
+const { QUEUE_URL, sqs } = require("./sqs");
+
+const sqsConsumer = Consumer.create({
+  queueUrl: QUEUE_URL,
+  handleMessage: async (message) => {
+    try {
+      const { key } = JSON.parse(message.Body);
+      await imageDb.update(
+        { url: `photoservice/${key}` },
+        { status: "resized" }
+      );
+    } catch (error) {
+      console.error("Something went wrong");
+      console.log(error.message);
+    }
+  },
+  sqs,
+});
+
+sqsConsumer.on("error", (err) => {
+  // TODO: Use winston for this
+  console.log("Error");
+  console.error(err);
+});
+
+sqsConsumer.on("processing_error", (err) => {
+  console.log("Error processing message from queue");
+  console.error(err);
+});
+
+module.exports = sqsConsumer;
