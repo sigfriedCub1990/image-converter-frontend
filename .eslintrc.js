24e25133c10e07f54862442dfd5f1aff4547170d
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-01-13 15:32:30 +0300

    chore: Add Jest to ESLint globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
