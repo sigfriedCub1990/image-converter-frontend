652c2eaca523df363d342f75dac702969cd51036
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-01-13 13:46:47 +0300

    feat: Associate Images and Users

--- app/use-cases/user/associate-image-user.js
+++ app/use-cases/user/associate-image-user.js
@@ -1,8 +1,21 @@
-function makeAssociateImageAndUser({ userDb }) {
-  return async ({ userId, imageId }) => {
-    const user = await userDb.findOne({ _id: userId });
-    user.images.push(imageId);
-    await userDb.update({ _id: user._id }, user);
+function makeAssociateImageAndUser({ userDb, imageDb }) {
+  return async ({ userId, imageUUID }) => {
+    try {
+      const insertedImage = await imageDb.insert({
+        url: `photoservice/${imageUUID}`,
+        status: "enqueued",
+      });
+
+      const user = await userDb.findOne({ _id: userId });
+      const imageId = insertedImage._id.toString();
+
+      user.images.push(imageId);
+      await userDb.update({ _id: user._id }, user);
+
+      return imageId;
+    } catch (error) {
+      throw new Error(error.message);
+    }
   };
 }
 
--- app/use-cases/user/index.js
+++ app/use-cases/user/index.js
@@ -1,8 +1,11 @@
-const { usersDb } = require("../../data-access");
+const { usersDb, imageDb } = require("../../data-access");
 
 const makeAssociateImageAndUser = require("./associate-image-user");
 
-const associateImageAndUser = makeAssociateImageAndUser({ userDb: usersDb });
+const associateImageAndUser = makeAssociateImageAndUser({
+  userDb: usersDb,
+  imageDb,
+});
 
 module.exports = Object.freeze({
   associateImageAndUser,
